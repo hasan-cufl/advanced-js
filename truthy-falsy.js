22bc979c914688fe043e7215bbcf77943a6d7e04
hasan-cugl <faridulhasancufl@gmail.com>
2020-10-22 20:45:09 +0600

    true vs false

---
+++ truthy-falsy.js
@@ -0,0 +1,34 @@
+// Truthy and Falsy values
+
+//  falsy values
+// 1: false
+// 2: 0
+// 3: ""
+// 4: undefined
+// 5: null
+// 6: NaN
+
+//  truthy values
+// 1: "false"
+// 2: " "
+// 3: "0"
+// 4: []
+
+// example 1
+const number = 0;
+if(number){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+
+// example2
+const name = '';
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
